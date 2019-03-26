0e81df0ad227ff6176c03170042b3751a0e3cf20
Teche J Melancon <tmelan9@lsu.edu>
2019-03-26 11:50:19 -0500

    upload

---
+++ map2/webmap2.js
@@ -0,0 +1,14 @@
+let myMap = L.map('TecheMap2').setView([32.18, -99.14], 4)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(myMap)
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer', {
+  layers: '1',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA'
+}).addTo(myMap)
