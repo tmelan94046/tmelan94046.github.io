19b40b09b24c0ef991085ac758665bf775b37125
tmelan94046 <48726564+tmelan94046@users.noreply.github.com>
2019-03-26 10:32:50 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -9,7 +9,12 @@ let polygon=L.polygon([
 ]).addTo(myMap)
 polygon.bindPopup('A polygon')
 marker.bindPopup('A marker')
+// create a red polyline from an array of LatLng points
 var latlngs = [
     [45.51, -122.68],
     [37.77, -122.43],
     [34.04, -118.2]
+];
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+// zoom the map to the polyline
+map.fitBounds(polyline.getBounds());
