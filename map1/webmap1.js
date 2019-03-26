be709c6ecaa73ee4da64a0231cfbc38f0784ae3d
tmelan94046 <48726564+tmelan94046@users.noreply.github.com>
2019-03-26 10:54:08 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,8 +1,5 @@
 let myMap = L.map('TecheMap1').setView([32.18, -99.14], 4)
-var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
 let marker=L.marker([30.411912, -91.183643]).addTo(myMap)
 let polygon=L.polygon([
   [30.417498, -91.180412],
