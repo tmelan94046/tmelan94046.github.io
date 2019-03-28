e68b358fd6eb3064ca56040ed9ceda28d35eedaf
Teche J Melancon <tmelan9@lsu.edu>
2019-03-28 11:02:53 -0500

    upload

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,9 +1,5 @@
 let myMap = L.map('TecheMap2').setView([32.18, -99.14], 4)
-var Thunderforest_TransportDark = L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey={apikey}', {
-	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	apikey: '<your apikey>',
-	maxZoom: 22
-});.addTo(myMap)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
