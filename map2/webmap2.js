6b8580982a6e7e5c985e252f179b7e28787d0a31
Teche <tmelan9@lsu.edu>
2019-04-01 17:40:20 -0500

    upload

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,5 +1,5 @@
 let myMap = L.map('TecheMap2').setView([32.18, -99.14], 4)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
+L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(myMap)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
