05b53ed4ef7c6d5ec2bc9488f7ae2e10a46022a2
Teche <tmelan9@lsu.edu>
2019-05-03 11:57:25 -0500

    upload

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,12 +1,12 @@
-let myMap = L.map('TecheMap3').setView([32.18, -99.14], 4)
+let myMap = L.map('TecheMap3').setView([30.44, -91.188], 10)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(myMap)
 let facilitylocations = 'https://opendata.arcgis.com/datasets/f14b547d30124f37821aa111efa46b72_0.geojson'
 
 jQuery.getJSON(facilitylocations, function (data) {
   let facilitystyle = function (feature) {
-    let age = feature.properties.MED_AGE // get the current state's Median Age attribute
-    let facilitycolor = 'olive' // let the initial color be a darker green
-    if ( age < 38 ) { facilitycolor = 'green' } // if the state's median age is less than the average, color it a lighter green
+    let Agency = feature.properties. AGENCY // agency of the public facility
+    let facilitycolor = 'blue' // let the initial color be a darker green
+    if ( Agency == DEPARTMENT OF ENVIRONMENTAL SERVICES ) { facilitycolor = 'green' }
     return {
       color: facilitycolor, //use the color variable above for the value
       weight: 1,
@@ -19,7 +19,7 @@ jQuery.getJSON(facilitylocations, function (data) {
     onEachFeature: createpopup
   }
 
-  L.geoJSON(data, facilitygeojsonoptions).addTo(mymap)
+  L.geoJSON(data, facilitygeojsonoptions).addTo(myMap)
 })
 
 let createpopup = function (feature, layer) {
