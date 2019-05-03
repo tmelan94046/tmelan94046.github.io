9b4a26a5716ea7fde051aef500a140373f4bec03
Teche <tmelan9@lsu.edu>
2019-05-02 21:53:45 -0500

    upload

---
+++ map3/webmap3.js
@@ -0,0 +1,35 @@
+let myMap = L.map('TecheMap3').setView([32.18, -99.14], 4)
+L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(myMap)
+let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+jQuery.getJSON(stateDemographicsUrl, function (data) {
+  L.geoJSON(data).addTo(stateMap)
+})
+L.geoJSON(data, { style: { color: 'green' } }).addTo(stateMap)
+//              |---------object 1---------|
+//                       |---object 2-----|
+L.geoJSON(data, { style: { color: 'green' }}).addTo(stateMap)
+let stateStyle = { color: 'green' }
+let stateGeojsonOptions = { style: stateStyle }
+L.geoJSON(data, stateGeojsonOptions).addTo(stateMap)
+let stateStyle = function (feature) {
+  let age = feature.properties.MED_AGE // get the current state's Median Age attribute
+  let stateColor = 'olive' // let the initial color be a darker green
+  if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+  return {
+    color: stateColor, //use the color variable above for the value
+    weight: 1,
+    fillOpacity: 0.2
+  }
+}
+let stateGeojsonOptions = {
+style: stateStyle,
+onEachFeature: onEachFeature
+}
+let onEachFeature = function (feature, layer) {
+   // contents of the function
+}
+let onEachFeature = function (feature, layer) {
+  let name = feature.properties.STATE_NAME
+  let age = feature.properties.MED_AGE
+  layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+}
