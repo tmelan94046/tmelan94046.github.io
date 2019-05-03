baf3525f92cdbb35f43ab32e0f86e3143d5461c8
Teche <tmelan9@lsu.edu>
2019-05-02 23:00:21 -0500

    upload

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,25 +1,28 @@
 let myMap = L.map('TecheMap3').setView([32.18, -99.14], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(myMap)
-let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
-jQuery.getJSON(stateDemographicsUrl, function (data) {
-  L.geoJSON(data).addTo(stateMap)
-})
-let stateStyle = function (feature) {
-  let age = feature.properties.MED_AGE // get the current state's Median Age attribute
-  let stateColor = 'olive' // let the initial color be a darker green
-  if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
-  return {
-    color: stateColor, //use the color variable above for the value
-    weight: 1,
-    fillOpacity: 0.2
+let facilitylocations = 'https://opendata.arcgis.com/datasets/f14b547d30124f37821aa111efa46b72_0.geojson'
+
+jQuery.getJSON(facilitylocations, function (data) {
+  let facilitystyle = function (feature) {
+    let age = feature.properties.MED_AGE // get the current state's Median Age attribute
+    let facilitycolor = 'olive' // let the initial color be a darker green
+    if ( age < 38 ) { facilitycolor = 'green' } // if the state's median age is less than the average, color it a lighter green
+    return {
+      color: facilitycolor, //use the color variable above for the value
+      weight: 1,
+      fillOpacity: 0.2
+    }
   }
-}
-let stateGeojsonOptions = {
-style: stateStyle,
-onEachFeature: onEachFeature
-}
-L.geoJSON(data, stateGeojsonOptions).addTo(stateMap)
-let onEachFeature = function (feature, layer) {
+
+  let facilitygeojsonoptions = {
+    style: facilitystyle,
+    onEachFeature: createpopup
+  }
+
+  L.geoJSON(data, facilitygeojsonoptions).addTo(mymap)
+})
+
+let createpopup = function (feature, layer) {
   let name = feature.properties.STATE_NAME
   let age = feature.properties.MED_AGE
   layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
